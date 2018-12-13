ed5b39211daa1f9b304c7bc72ec50779f6c5f821
Endogen <081081@gmx.net>
2018-12-14 00:00:42 +0100

    Added '/g' command for global stats

--- opencryptobot/plugins/global.py
+++ opencryptobot/plugins/global.py
@@ -1,3 +1,9 @@
+import io
+import plotly.io as pio
+import plotly.graph_objs as go
+import opencryptobot.emoji as emo
+
+from io import BytesIO
 from telegram import ParseMode
 from opencryptobot.plugin import OpenCryptoPlugin
 from opencryptobot.api.coingecko import CoinGecko
@@ -11,21 +17,112 @@ class Global(OpenCryptoPlugin):
     @OpenCryptoPlugin.send_typing
     @OpenCryptoPlugin.save_data
     def get_action(self, bot, update, args):
-        #if not args:
-        #    update.message.reply_text(
-        #        text=f"Usage:\n{self.get_usage()}",
-        #        parse_mode=ParseMode.MARKDOWN)
-        #    return
+        if not args:
+            update.message.reply_text(
+                text=f"Usage:\n{self.get_usage()}",
+                parse_mode=ParseMode.MARKDOWN)
+            return
 
-        response = CoinGecko().get_global()
+        sub_cmd = args[0]
+
+        coin = str()
+        if len(args) > 1:
+            coin = args[1]
+
+        if not sub_cmd:
+            update.message.reply_text(
+                text=f"Usage:\n{self.get_usage()}",
+                parse_mode=ParseMode.MARKDOWN)
+            return
+
+        msg = str()
+        res = CoinGecko().get_global()
+
+        m_cap_usd = "{0:,.0f}".format(res["total_market_cap"]["usd"])
+        m_cap_eur = "{0:,.0f}".format(res["total_market_cap"]["eur"])
+
+        vol_usd = "{0:,.0f}".format(res["total_volume"]["usd"])
+        vol_eur = "{0:,.0f}".format(res["total_volume"]["eur"])
+
+        m_cap_per = res["market_cap_percentage"]
+
+        # Total Market Capital
+        if sub_cmd.lower() == "mcap":
+            if coin:
+                if coin in res["total_market_cap"]:
+                    mcap = "{0:,.0f}".format(res["total_market_cap"][coin])
+                    msg = f"Total Market Capital\n`{coin.upper()}: {mcap}`"
+            else:
+                msg = f"Total Market Capital\n" \
+                      f"`USD: {m_cap_usd}`\n" \
+                      f"`EUR: {m_cap_eur}`"
+
+        # Total Volume
+        elif sub_cmd.lower() == "vol":
+            if coin:
+                if coin in res["total_volume"]:
+                    vol = "{0:,.0f}".format(res["total_volume"][coin])
+                    msg = f"Total Volume\n`{coin.upper()}: {vol}`"
+            else:
+                msg = f"Total Volume (24h)\n" \
+                      f"`USD: {vol_usd}`\n" \
+                      f"`EUR: {vol_eur}`"
+
+        elif sub_cmd.lower() == "dom":
+            labels = list()
+            values = list()
+
+            msg = "Dominance (Market Capital)\n"
+            for key in sorted(m_cap_per, key=m_cap_per.get, reverse=True):
+                labels.append(key.upper())
+                values.append(m_cap_per[key])
+
+                value = "{0:.2f}".format(m_cap_per[key])
+                tst = "{:>13}".format(f"{value}%")
+                tst = key.upper() + tst[len(key):]
+                msg += f"`{tst}`\n"
+
+            labels.append("[Other]")
+            values.append(100 - sum(values))
+
+            layout = go.Layout(
+                paper_bgcolor='rgb(233,233,233)',
+                plot_bgcolor='rgb(233,233,233)',
+                autosize=True,
+                width=800,
+                height=600,
+                margin=go.layout.Margin(
+                    l=10,
+                    r=10,
+                    b=10,
+                    t=10,
+                    pad=4
+                )
+            )
+
+            trace = go.Pie(labels=labels, values=values)
+            fig = go.Figure(data=[trace], layout=layout)
+
+            update.message.reply_photo(
+                photo=io.BufferedReader(BytesIO(pio.to_image(fig, format="webp"))),
+                caption=msg,
+                parse_mode=ParseMode.MARKDOWN)
+
+            return
+
+        if not msg:
+            msg = f"{emo.ERROR} Can't retrieve data for *{coin.upper()}*"
 
         update.message.reply_text(
-            text=str(response),
-            parse_mode=ParseMode.HTML,
-            disable_web_page_preview=True)
+            text=msg,
+            parse_mode=ParseMode.MARKDOWN)
 
     def get_usage(self):
-        return f"`/{self.get_cmd()} <coin>`"
+        return f"`" \
+               f"/{self.get_cmd()} mcap (<coin>)\n" \
+               f"/{self.get_cmd()} vol (<coin>)\n" \
+               f"/{self.get_cmd()} dom (<coin>)" \
+               f"`"
 
     def get_description(self):
-        return "Coin description"
+        return "Global crypto data"
