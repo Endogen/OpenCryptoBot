e50326ab022c59752fb3d4ad8ffe3f8c3dfaa77b
Endogen <081081@gmx.net>
2019-01-01 23:10:48 +0100

    Better error handling

--- opencryptobot/api/cryptocompare.py
+++ opencryptobot/api/cryptocompare.py
@@ -1,3 +1,4 @@
+import logging
 import requests
 
 
@@ -11,21 +12,43 @@ class CryptoCompare(object):
             self._base_url = base_url
 
     def get_historical_ohlcv_daily(self, fsym, tsym, limit):
-        url_data = f"histoday?fsym={fsym}&tsym={tsym}&limit={limit}"
-        self.response = requests.get(f"{self._base_url}{url_data}").json()
-        return self.response
+        try:
+            url_data = f"histoday?fsym={fsym}&tsym={tsym}&limit={limit}"
+            self.response = requests.get(f"{self._base_url}{url_data}")
+            self.response.raise_for_status()
+            return self.response.json()
+        except Exception as ex:
+            return self._handle_error(ex)
 
     def get_historical_ohlcv_hourly(self, fsym, tsym, limit):
-        url_data = f"histohour?fsym={fsym}&tsym={tsym}&limit={limit}"
-        self.response = requests.get(f"{self._base_url}{url_data}").json()
-        return self.response
+        try:
+            url_data = f"histohour?fsym={fsym}&tsym={tsym}&limit={limit}"
+            self.response = requests.get(f"{self._base_url}{url_data}")
+            self.response.raise_for_status()
+            return self.response.json()
+        except Exception as ex:
+            return self._handle_error(ex)
 
     def get_historical_ohlcv_minute(self, fsym, tsym, limit):
-        url_data = f"histominute?fsym={fsym}&tsym={tsym}&limit={limit}"
-        self.response = requests.get(f"{self._base_url}{url_data}").json()
-        return self.response
+        try:
+            url_data = f"histominute?fsym={fsym}&tsym={tsym}&limit={limit}"
+            self.response = requests.get(f"{self._base_url}{url_data}")
+            self.response.raise_for_status()
+            return self.response.json()
+        except Exception as ex:
+            return self._handle_error(ex)
 
     def get_coin_general_info(self, fsyms, tsym):
-        url_data = f"coin/generalinfo?fsyms={fsyms}&tsym={tsym}"
-        self.response = requests.get(f"{self._base_url}{url_data}").json()
-        return self.response
+        try:
+            url_data = f"coin/generalinfo?fsyms={fsyms}&tsym={tsym}"
+            self.response = requests.get(f"{self._base_url}{url_data}")
+            self.response.raise_for_status()
+            return self.response.json()
+        except Exception as ex:
+            return self._handle_error(ex)
+
+    def _handle_error(self, ex):
+        logging.error(repr(ex))
+        logging.error(f"Request URL: {self.response.url}")
+        logging.error(f"Response Status Code: {self.response.status_code}")
+        return None
