6fc474999b0a9b5e99694da828a31698057316b3
Endogen <081081@gmx.net>
2019-01-31 23:10:51 +0100

    Added skeleton for 'admin' command

--- opencryptobot/plugins/admin.py
+++ opencryptobot/plugins/admin.py
@@ -1,7 +1,6 @@
-import os
-import sys
-import time
+import logging
 import opencryptobot.emoji as emo
+import opencryptobot.utils as utl
 
 from opencryptobot.plugin import OpenCryptoPlugin
 from opencryptobot.config import ConfigManager as Cfg
@@ -15,9 +14,16 @@ class Admin(OpenCryptoPlugin):
     @OpenCryptoPlugin.only_owner
     @OpenCryptoPlugin.send_typing
     def get_action(self, bot, update, args):
-        # TODO: Set parameters in config
-        # TODO: Get latest bot stats
-        pass
+        if not args:
+            return
+
+        sub_cmd = args[0]
+
+        if "config" == sub_cmd.lower():
+            keywords = utl.get_keywords(args)
+
+            for key, value in keywords.items():
+                self._change_cfg(key, value)
 
     def get_usage(self):
         return None
@@ -27,3 +33,6 @@ class Admin(OpenCryptoPlugin):
 
     def get_category(self):
         return None
+
+    def _change_cfg(self, key, value):
+        pass
