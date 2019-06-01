11962bf86fbb77f7ce88fba91d95089bd394cd5f
Endogen <081081@gmx.net>
2019-06-02 01:09:42 +0200

    Added new API (Wallets) for CryptoCompare

--- opencryptobot/api/cryptocompare.py
+++ opencryptobot/api/cryptocompare.py
@@ -5,12 +5,15 @@ import requests
 class CryptoCompare(object):
 
     _base_url = 'https://min-api.cryptocompare.com/data/'
+    _token = None
 
     response = None
 
-    def __init__(self, base_url=None):
+    def __init__(self, base_url=None, token=None):
         if base_url:
             self._base_url = base_url
+        if token:
+            self._token = token
 
     def _request(self, url):
         try:
@@ -19,6 +22,10 @@ class CryptoCompare(object):
             return json.loads(self.response.content.decode('utf-8'))
         except Exception as e:
             raise e
+
+    def load_key(self, path):
+        with open(path, 'r') as f:
+            self._token = f.readline().strip()
 
     def get_historical_ohlcv_daily(self, fsym, tsym, limit):
         url_data = f"histoday?fsym={fsym}&tsym={tsym}&limit={limit}"
@@ -34,4 +41,8 @@ class CryptoCompare(object):
 
     def get_coin_general_info(self, fsyms, tsym):
         url_data = f"coin/generalinfo?fsyms={fsyms}&tsym={tsym}"
+        return self._request(f"{self._base_url}{url_data}")
+
+    def get_wallets_info(self):
+        url_data = f"wallets/general?api_key={self._token}"
         return self._request(f"{self._base_url}{url_data}")
