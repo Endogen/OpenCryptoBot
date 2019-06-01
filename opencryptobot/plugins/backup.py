88029c82967ecf4ac30f6729c209f7fdee4a4974
Endogen <081081@gmx.net>
2019-06-02 01:25:13 +0200

    Added sending of backup file after creation

--- opencryptobot/plugins/backup.py
+++ opencryptobot/plugins/backup.py
@@ -40,9 +40,10 @@ class Backup(OpenCryptoPlugin):
                     if os.path.isfile(path):
                         zf.write(path, os.path.relpath(path, base_path))
 
-        update.message.reply_text(
-            text=f"{emo.CHECK} Backup created",
-            parse_mode=ParseMode.MARKDOWN)
+        bot.send_document(
+            chat_id=update.effective_user.id,
+            caption=f"{emo.CHECK} Backup created",
+            document=open(os.path.join(base_dir, filename), 'rb'))
 
     def get_usage(self):
         return None
